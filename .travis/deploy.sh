f25dcc69539c452d7e71c826c61a475ff38f2887
NamHandsome <namthit98@gmail.com>
2019-05-09 17:36:10 +0700

    update

---
+++ .travis/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+chmod 600 .travis/id_rsa # Allow read access to the private key
+ssh-add .travis/id_rsa # Add the private key to SSH
+
+git config --global push.default simple
+git remote add deploy ssh://namthit98@$IP/$DEPLOY_DIR
+git push deploy master
