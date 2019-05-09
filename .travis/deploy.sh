411f199d9b38c93ea3992598672416f91505fcf0
NamHandsome <namthit98@gmail.com>
2019-05-09 18:19:32 +0700

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,3 +7,10 @@ ssh-add .travis/id_rsa # Add the private key to SSH
 git config --global push.default simple
 git remote add deploy ssh://namthit98@$IP/$DEPLOY_DIR
 git push deploy master
+
+# Skip this command if you don't need to execute any additional commands after deploying.
+ssh apps@$IP <<EOF
+  cd $DEPLOY_DIR
+  sudo docker-compose down
+  sudo docker-compose -f docker-compose.prod.yml up -d
+EOF
