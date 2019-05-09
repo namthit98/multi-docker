044b8b9856c651cbd4470b02d52c92b75e8d612b
NamHandsome <namthit98@gmail.com>
2019-05-09 17:00:36 +0700

    test branch

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,4 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-it("renders without crashing", () => {});
+it("renders without crashing", () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+it("test faild", () => {
+    expect(1).toBe(3);
+});
