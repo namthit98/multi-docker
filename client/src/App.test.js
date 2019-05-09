c85ecbc049109c0d97f9d49c2b1e727f6231bb9c
NamHandsome <namthit98@gmail.com>
2019-05-09 17:04:12 +0700

    update

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,12 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-it("renders without crashing", () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
-
 it("test faild", () => {
     expect(3).toBe(3);
 });
